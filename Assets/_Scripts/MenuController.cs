49d2e5f441045e4521909024966b00c77e19b1ae
KiranKaur <k.angel117@yahoo.com>
2016-04-01 00:16:49 -0400

    Build Menu for Game

---
+++ Assets/_Scripts/MenuController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+  
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void StartButtonClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void InstructionButtonClick()
+    {
+        SceneManager.LoadScene("Instruction");
+    }
+    public void BackButtonClick()
+    {
+        SceneManager.LoadScene("menu");
+    }
+    public void QuitButtonClick()
+    {
+        Application.Quit();
+    }
+}
